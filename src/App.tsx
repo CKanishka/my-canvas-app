a767cfb1a3ff166dbdb81e89b4090b87b9dbd64a
CKanishka <ckanishka24@gmail.com>
2024-05-28 23:41:42 +0530

    initialised react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import CanvasHome from "./pages/CanvasHome.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage/index.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CanvasHome />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <CanvasHome />
-      {/* <CanvasDraw ref={canvasRef} disabled lazyRadius={0} hideGrid /> */}
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
